f6e2d533e665d3ed29b6b9bf4025498b8c45aca9
Dawidowska <40340729@live.napier.ac.uk>
2020-02-11 23:31:50 +0000

    Adding Home page components

--- project/src/layouts/Home.js
+++ project/src/layouts/Home.js
@@ -5,69 +5,64 @@ export default class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          formControls: {
-            userName: {
-              value: ''
-            },
-            password: {
-              value: ''
-            }
-          }
+          userName: '',
+          password: ''
         };
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
       }
 
-      handleChange = event => {
-
-        const name = event.target.name;
-        const value = event.target.value;
-    
-        this.setState({
-          formControls: {
-            ...this.state.formControls,
-            [name]: {
-              ...this.state.formControls[name],
-              value
-            }
-          }
-        });
+      handleChange(event) {
+          this.setState({
+              [event.target.name]: event.target.value
+          });
       }
-    
+      
       handleSubmit(event) {
         alert(
-        this.state.formControls.userName.value +
-        '\n' +
-        this.state.formControls.password.value
+        "Login button works"
         );
         event.preventDefault();
       }
 
+      btnRegister(event) {
+          
+      }
+
       render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-           
-            <label>
-              Username:
-              <input
-                name="userName"
-                value={this.state.formControls.userName.value}
-                onChange={this.handleChange}
-              />
-            </label>
-    
-            <label>
-              Password:
-              <input
-                name="password"
-                value={this.state.formControls.password.value}
-                onChange={this.handleChange}
-              />
-            </label>
-    
-            <input type="submit" value="Submit" />
-          </form>
+        return (       
+          <div>
+              <form onSubmit={this.handleSubmit}>
+                  <h1>Q-METHODOLOGY</h1>
+                  
+                  <input 
+                    type="text"
+                    name="userName"
+                    placeholder="Username"
+                    value={this.state.userName}
+                    onChange={this.handleChange}
+                    required
+                  />
+
+                    <input 
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    required
+                  />
+
+                  <button type="submit">
+                    Login
+                  </button>
+              </form>
+
+                <button onSubmit={this.btnRegister} type="submit">
+                    Register
+                </button>
+          </div>
         );
       }
 }
