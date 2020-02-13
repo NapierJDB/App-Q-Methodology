02e642bcee865017428a28df1f78dfe59b19f275
callumbuttery <40408762@live.napier.ac.uk>
2020-02-13 15:22:15 +0000

    Adding using input on registration form

--- project/src/layouts/RegForm.js
+++ project/src/layouts/RegForm.js
@@ -1,98 +1,130 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 export default class RegForm extends React.Component {
-    constructor(props) {
-        super(props);
-        /* To register a user we are storing:
+  constructor(props) {
+    super(props);
+    /* To register a user we are storing:
            forename
            surname
            email
            password */
-        this.state = {
-            userName: '',
-            email: '',
-            password: '',
-            confirmPassword: ''
-        };
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    //I think this must be changed
-    handleChange = event => {
-        this.setState({ 
-            [event.target.name]: event.target.value
-        });
+    this.state = {
+      forename: "",
+      surname: "",
+      email: "",
+      password: ""
     };
 
-    handleSubmit = event => {
-        event.preventDefault();
-        const { userName, email, password, confirmPassword } = this.state;
+    /*     this.handleSubmit = this.handleSubmit.bind(this); */
+    this.handleChangeForename = this.handleChangeForename.bind(this);
+    this.handleChangeSurname = this.handleChangeSurname.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+  }
 
-        /*This is the JSON part
+  //functions for handling changes in variable values
+  handleChangeForename = event => {
+    this.setState({ forename: event.target.value });
+  };
+
+  handleChangeSurname = event => {
+    this.setState({ surname: event.target.value });
+  };
+
+  handleChangePassword = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleChangeEmail = event => {
+    this.setState({ email: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { forename, surname, email, password } = this.state;
+
+    /*This is the JSON part
         so the forname...etc must correspond to
         user input 
         atm they are hardcoded */
-        axios.post('https://www.one.barttest.me.uk/Project2/public/account/register',
-         {
-             forename: 'Anna',
-             surname: 'Banana',
-             email: 'email@op.pl',
-             password: 'banana password'
-         })
-         .then(function (resonse) {
-             console.log(resonse);
-         })
-         .catch(function (error) {
-             console.log(error);
-         });
-    }
+    axios
+      .post("https://www.one.barttest.me.uk/Project2/public/account/register", {
+        forename: "Anna",
+        surname: "Banana",
+        email: "email@op.pl",
+        password: "banana password"
+      })
+      .then(function(resonse) {
+        console.log(resonse);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    name="userName"
-                    placeholder="User name"
-                    value={this.state.userName}
-                    onChange={this.handleChange}
-                    required
-                />
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="forename"
+            placeholder="Forename"
+            forename={this.state.forename}
+            //function call
+            onChange={this.handleChangeForename}
+            required
+          />
 
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                    required
-                />
+          <input
+            type="text"
+            name="Surname"
+            placeholder="Surname"
+            surname={this.state.surname}
+            //function call
+            onChange={this.handleChangeSurname}
+            required
+          />
 
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    required
-                />
+          <input
+            type="email"
+            name="Email"
+            placeholder="Email"
+            email={this.state.email}
+            //function call
+            onChange={this.handleChangeEmail}
+            required
+          />
 
-                <input
-                    type="password"
-                    name="confirmPassword"
-                    placeholder="Password confirmation"
-                    value={this.state.confirmPassword}
-                    onChange={this.handleChange}
-                    required
-                />
+          <input
+            type="password"
+            name="Passwrod"
+            placeholder="Password "
+            confirmpassword={this.state.password}
+            //function call
+            onChange={this.handleChangePassword}
+            required
+          />
 
-                <button type="submit">Register</button>
-                </form>
-            </div>
-        )
-    }
+          <button
+            type="submit"
+            onClick={() => {
+              alert(
+                this.state.forename +
+                  "\n" +
+                  this.state.surname +
+                  "\n" +
+                  this.state.email +
+                  "\n" +
+                  this.state.password
+              );
+            }}
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
