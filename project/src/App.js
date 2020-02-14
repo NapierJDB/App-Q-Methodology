f4bfc8048ca3f0598ccf94c3a411645070bf750e
callumbuttery <40408762@live.napier.ac.uk>
2020-02-14 16:42:43 +0000

    adding admit panel page

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from './layouts/Home';
 import RegForm from './layouts/RegForm';
+import AdminPanel from "./layouts/AdminPanel";
 
 export default class App extends React.Component {
   render() {
     return (
-      <Home />
+      <AdminPanel />
     );
   }
 }
