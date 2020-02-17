65318442f92475b994712846a2a0e168a6bf8a91
Dawidowska <40340729@live.napier.ac.uk>
2020-02-17 13:23:37 +0000

    test

--- project/src/layouts/Home.js
+++ project/src/layouts/Home.js
@@ -66,6 +66,15 @@ export default class Home extends React.Component {
                     required
                   />
 
+<input 
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    required
+                  />
+
                     <input 
                     type="password"
                     name="password"
